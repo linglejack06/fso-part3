aa008a195a6e309e0fdfc381efd1907f765ef1a2
Jack Lingle <captainjack0818@gmail.com>
2023-06-17 17:13:44 -0400

    copy notes frontend from part 2 into part 3

---
+++ lesson-frontend/src/services/notes.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+const baseUrl = 'http://localhost:3000/notes';
+
+const getAll = () => {
+  const response = axios.get(baseUrl)
+  const nonExisting = {
+    id: 10000,
+    content: 'not saved to server',
+    important: true,
+  }
+  return response.then((response) => response.data.concat(nonExisting));
+}
+
+const create = (newObject) => {
+  const response = axios.post(baseUrl, newObject)
+  return response.then((response) => response.data);
+}
+
+const update = (id, newObject) => {
+  const response = axios.put(`${baseUrl}/${id}`, newObject)
+  return response.then((response) => response.data);
+}
+const noteService = { getAll, create, update, }
+export default noteService;
