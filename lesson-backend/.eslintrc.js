5e21881d521939c51b62b55c4b712943f80108a0
Jack Lingle <captainjack0818@gmail.com>
2023-06-20 18:41:08 -0400

    add eslint

--- lesson-backend/.eslintrc.js
+++ lesson-backend/.eslintrc.js
@@ -1,18 +1,40 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-    node: true,
+  'env': {
+    'node': true,
+    'commonjs': true,
+    'es2021': true
   },
-  extends: 'airbnb-base',
-  overrides: [
+  'extends': 'eslint:recommended',
+  'overrides': [
   ],
-  parserOptions: {
-    ecmaVersion: 'latest',
+  'parserOptions': {
+    'ecmaVersion': 'latest'
   },
-  rules: {
+  'rules': {
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
     'no-console': 'off',
-    'import/no-extraneous-dependencies': 'off',
-  },
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
 };
